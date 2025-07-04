55ae97f94cf59dc5a9475bf4108e33eb824cf1d9
Jerald Vega <j.vega90938@gmail.com>
2025-07-04 10:07:15 -0400

    accept file path as CLI argument instead of hardcoding

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words, count_characters, sort_char_counts
 
 
@@ -8,24 +9,29 @@ def get_book_text(filepath):
 
 
 def main():
-    # print(get_book_text("./books/frankenstein.txt"))
-    book = get_book_text("./books/frankenstein.txt")
-    total_words = count_words(book)
-    char_counts = count_characters(book)
-    sorted_char_report = sort_char_counts(char_counts)
+    if (len(sys.argv) != 2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        # print(get_book_text("./books/frankenstein.txt"))
+        book = get_book_text(sys.argv[1])
+        total_words = count_words(book)
+        char_counts = count_characters(book)
+        sorted_char_report = sort_char_counts(char_counts)
 
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}")
 
-    print("----------- Word Count ----------")
-    print(f"Found {total_words} total words")
+        print("----------- Word Count ----------")
+        print(f"Found {total_words} total words")
 
-    print("--------- Character Count -------")
-    for item in sorted_char_report:
-        if item['char'].isalpha():
-            print(f"{item['char']}: {item['num']}")
+        print("--------- Character Count -------")
+        for item in sorted_char_report:
+            if item['char'].isalpha():
+                print(f"{item['char']}: {item['num']}")
 
-    print("============= END ===============")
+        print("============= END ===============")
+    
 
 
 main()
